572d38f397d2f21233e3ecca9e8fdbe914a60d98
Felipe <felmartins1985@gmail.com>
2022-07-23 17:36:39 -0300

    feat: requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+SELECT titulo AS nome_musica,
+CASE
+			WHEN titulo LIKE "%Streets%" THEN REPLACE(titulo,'Streets',"Code Review")
+            WHEN titulo LIKE  "%Her Own%" THEN REPLACE(titulo,'Her Own',"Trybe")
+            WHEN titulo LIKE "%Inner Fire%" THEN REPLACE(titulo,'Inner Fire',"Project")
+            WHEN titulo LIKE  "%Silly%" THEN REPLACE(titulo,'Silly',"Nice")
+            WHEN titulo LIKE "%Circus%"  THEN REPLACE(titulo,'Circus',"Pull Request")
+	ELSE titulo
+END AS novo_nome
+FROM SpotifyClone.musica
+WHERE titulo LIKE '%Streets%' OR titulo LIKE '%Her Own%' 
+OR titulo LIKE '%Inner Fire%' OR titulo LIKE '%Silly%'
+OR titulo LIKE '%Circus%'
+ORDER BY novo_nome;
